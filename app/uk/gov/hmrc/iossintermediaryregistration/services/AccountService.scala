202ae15ea269c181a3d606442245573df36a74cb
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-10-28 12:11:42 +0000

    create AccountService

---
+++ app/uk/gov/hmrc/iossintermediaryregistration/services/AccountService.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.iossintermediaryregistration.services
+
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.iossintermediaryregistration.connectors.EnrolmentsConnector
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class AccountService @Inject()(
+                                enrolmentsConnector: EnrolmentsConnector
+                              )(implicit ec: ExecutionContext) {
+
+  def getLatestAccount(userId: String)(implicit hc: HeaderCarrier): Future[Option[String]] = {
+    enrolmentsConnector.es2(userId).map {
+      case Right(accounts) =>
+        accounts.enrolments
+          .filter(_.activationDate.isDefined)
+          .maxBy(_.activationDate.get)
+          .identifiers
+          .find(_.key == "IOSSNumber")
+          .map(_.value)
+      case Left(_) => None
+    }
+  }
+}
